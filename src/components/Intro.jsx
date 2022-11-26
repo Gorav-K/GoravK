96c5c63bf1e522edea932c65e94da8d1e4f02a89
Gorav-K <78222737+Gorav-K@users.noreply.github.com>
2022-11-26 05:04:10 -0500

    init vite project

---
+++ src/components/Intro.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+function Intro() {
+   return (
+      <div >
+         <div className="max-w-5xl w-11/12 mx-auto">
+            
+            <div className="flex items-center justify-center flex-col text-center pt-20 pb-6" >
+               <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold"><span role="img" aria-label="wave">👋</span> Hi, I am Gorav Karwer.</h1>
+               <br></br>
+               <p className="text-lg max-w-3xl litems-center justify-center mb-6 font-bold">
+               I am currently in my third-year studying Computer Science at John Abbott College, in Montreal, Canada. <br></br>
+               <br></br>
+               I explored many things before I became fascinated with computer science! I began by studying engineer at John Abbott College before changing track to study Science. Once I discovered the keys, my passion for computer science grew. <br></br>
+               <br></br>
+               I have excelled in game programming, database and web development, where I got to utilize my creativity and problem-solving skills while working on various academic projects. I am comfortable working as part of a team or independently. I am confident that my ambition, commitment and ongoing determination to learn more about the field are assets I can bring to the table.
+ </p>
+            </div>
+         </div>
+      </div>
+
+   )
+}
+
+export default Intro;
---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+   {
+      title: 'Bananabank 🍌🏦',
+      imgUrl: '/assets/terminal-website.jpg',
+      stack: ['HTML', 'CSS', 'JavaScript'],
+      link: 'https://fkcodes.com',
+   },
+   {
+      title: 'Cinemaware 🎥',
+      imgUrl: '/assets/cinemaware.png',
+      stack: ['Bootstrap', 'Sql', 'Node.js','Docker'],
+      link: 'https://github.com/rafdeguzman/CinemaWareWebsite',
+   },
+   {
+      title: 'Dino D-evolution 🦖',
+      imgUrl: '/assets/dino.png',
+      stack: ['Teachable Machine', 'p5.js', 'JavaScript', 'HTML', 'CSS'],
+      link: 'https://youtu.be/A8eegxL82PI',
+   },
+]
