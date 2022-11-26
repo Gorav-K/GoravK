8d045b6885936f212913e5463c5487a75f85c6f9
Gorav-K <78222737+Gorav-K@users.noreply.github.com>
2022-11-26 05:26:28 -0500

    update portfolio .js with new path

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
    {
       title: 'Bananabank 🍌🏦',
-      imgUrl: '/assets/terminal-website.jpg',
+      imgUrl: 'GoravK/assets/terminal-website.jpg',
       stack: ['HTML', 'CSS', 'JavaScript'],
       link: 'https://fkcodes.com',
    },
    {
       title: 'Cinemaware 🎥',
-      imgUrl: '/assets/cinemaware.png',
+      imgUrl: 'GoravK/assets/cinemaware.png',
       stack: ['Bootstrap', 'Sql', 'Node.js','Docker'],
       link: 'https://github.com/rafdeguzman/CinemaWareWebsite',
    },
    {
       title: 'Dino D-evolution 🦖',
-      imgUrl: '/assets/dino.png',
+      imgUrl: 'GoravK/assets/dino.png',
       stack: ['Teachable Machine', 'p5.js', 'JavaScript', 'HTML', 'CSS'],
       link: 'https://youtu.be/A8eegxL82PI',
    },
