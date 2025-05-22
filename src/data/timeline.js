b70c43f36d7e09b4f2d16ee08e89f2315e82333c
Gorav <78222737+Gorav-K@users.noreply.github.com>
2025-05-22 04:01:39 +0000

    Remove the exp

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,23 +1,23 @@
 export default [
-   {
-      year: 'Jun 2022 — Present',
-      title: 'Warehouse Supervisor, Ilagiisaq - FCNQ',
-      duration: '6 months',
-      details: 
-         " I supervise, train, direct and evaluate employees and I document and process paperwork for dangerous goods.",
-   },
-   {
-		year: 'Aug 2019 — Jun 2022',
-		title: 'Warehouse Supervisor, Ilagiisaq - FCNQ',
-		duration: '2 years 11 months',
-		details:
-		' I distributed, packaged, and labelled goods according to established policies and printed the various computer documents (packing slips) necessary to document the contents of the boxes of merchandise.',
-	},
-	{
-		year: 'Feb 2021 — Present',
-		title: "Laboratory Attendant, John Abbott College",
-		duration: '1 year 10 months',
-		details:
-			'I organize events attended by 100+ students.',
-	},
+//    {
+//       year: 'Jun 2022 — Present',
+//       title: 'Warehouse Supervisor, Ilagiisaq - FCNQ',
+//       duration: '6 months',
+//       details: 
+//          " I supervise, train, direct and evaluate employees and I document and process paperwork for dangerous goods.",
+//    },
+//    {
+// 		year: 'Aug 2019 — Jun 2022',
+// 		title: 'Warehouse Supervisor, Ilagiisaq - FCNQ',
+// 		duration: '2 years 11 months',
+// 		details:
+// 		' I distributed, packaged, and labelled goods according to established policies and printed the various computer documents (packing slips) necessary to document the contents of the boxes of merchandise.',
+// 	},
+// 	{
+// 		year: 'Feb 2021 — Present',
+// 		title: "Laboratory Attendant, John Abbott College",
+// 		duration: '1 year 10 months',
+// 		details:
+// 			'I organize events attended by 100+ students.',
+// 	},
 ]
